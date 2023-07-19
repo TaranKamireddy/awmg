8a92ad812f251ef7fef7ce35cb4c9ec2a825a8f7
Brian Tran <briantra23@gmail.com>
2023-07-19 18:24:38 -0400

    you saw what i did

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import placeholder from "../images/finance-hero.png";
 export default function Home() {
   return(
     <>
-      <div className="bruh">
+      <div className="header">
         <h1>American Wealth Management Group</h1>
         <img src={placeholder}></img>
       </div>
-      <h1>About Us</h1>
-      <p>bruh i love eating ass</p>
+      <div className="statement">
+        <h1>Discover AWMG</h1>
+        <p>We are a group of financial advisors eager to assist you</p>
+      </div>
     </>
   );
 }
