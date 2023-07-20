3390ce1c540e5a92dfe7b3ff7026a866bb59bdfb
Meluhn <tarankamireddy@gmail.com>
2023-07-20 19:15:21 -0400

    Spacing between divs
    no idea why there is a gap between the img and next div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,18 @@ import placeholder from "../images/finance-hero.png";
 
 export default function Home() {
   return(
-    <>
+    <div>
       <div className="header">
         <h1>American Wealth Management Group</h1>
-        <img src={placeholder}></img>
+        <img src={placeholder} alt="" draggable="false"></img>
       </div>
       <div className="statement">
         <h1>Discover AWMG</h1>
         <p>We are a group of financial advisors eager to assist you</p>
       </div>
-    </>
+      <div className="section">
+        <h1>Pls join our company</h1>
+      </div>
+    </div>
   );
 }
