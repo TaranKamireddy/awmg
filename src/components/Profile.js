d6e88c30c5a586f3adc33cba4fce6fc010c4cf42
Brian Tran <briantra23@gmail.com>
2023-07-22 17:26:03 -0400

    yuip ypup

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import testFace from "../images/testFace.png"
 import { FaPlus } from "react-icons/fa";
-import "../About.css"
+import "../pages/About.css"
+import Modal from "./Modal.js"
 
 export default function Profiles() {
 
@@ -10,9 +11,13 @@ const position = "Vice President"
   return (
     <div className="profile">
         <img src={testFace} alt="" draggable="false"></img>
-        <h1>{name}</h1>
-        <h2>{position}</h2>
-        <button><FaPlus style={{color: "#003B9F"}}/></button>
+        <div className="test">
+          <div className="verticalText">
+            <h1>{name}</h1>
+            <h2>{position}</h2>
+          </div>
+          <button ><FaPlus style={{color: "#003B9F"}}/></button>
+        </div>
     </div>
   )
 }
