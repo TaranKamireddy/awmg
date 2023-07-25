2abb0e57307226212d6afbe05ca1dc8c71b67821
Meluhn <tarankamireddy@gmail.com>
2023-07-25 16:32:55 -0400

    yuh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import "./Home.css"
 import Background from "../components/Background"
+import fadvisor from "../images/fadvisor.jpg"
 
 export default function Home() {
   return(
@@ -9,8 +10,13 @@ export default function Home() {
         <h1>American Wealth Management Group</h1>
       </div>
       <div className="statement">
-        <h1>Discover AWMG</h1>
-        <p>We are a group of financial advisors eager to assist you</p>
+        <div className="statement-text">
+          <h1>Discover AWMG</h1>
+          <p>We are a group of financial advisors eager to assist you</p>
+        </div>
+        <div className="statement-image">
+          <img src={fadvisor} alt="financial advisor" draggable="false" />
+        </div>
       </div>
       <div className="section">
         <h1>Pls join our company</h1>
