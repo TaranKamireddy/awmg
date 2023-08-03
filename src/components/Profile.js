b80ba77e72f893e6ed1465c2a4ea98a7a99b22a9
Brian Tran <briantra23@gmail.com>
2023-08-03 11:39:13 -0400

    taran

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,9 @@
 import { useState } from "react"
 import { FaPlus } from "react-icons/fa";
 import "../pages/About.css"
-import Modal from "./Modal.js"
+import Modal from 'react-modal';
 
-export default function Profile({name, position, description, image}) {
-  const [openModal, setOpenModal] = useState(false)
+export default function Profile({openModal, modalIsOpen, afterOpenModal, closeModal, name, description, image, position}) {
 
   return (
     <div className="profile">
@@ -14,9 +13,24 @@ export default function Profile({name, position, description, image}) {
             <h1>{name}</h1>
             <h2>{position}</h2>
           </div>
-          <button onClick={() => {setOpenModal(true)}}><FaPlus style={{color: "#003B9F"}}/></button>
+          <button onClick={openModal}><FaPlus style={{color: "#003B9F"}}/></button>
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+          >
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+        </Modal>
         </div>
-        {openModal && <Modal closeModal={setOpenModal} name={name} description={description}/>}
     </div>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,27 @@
 import Profile from "../components/Profile.js"
 import "./About.css"
 import testFace from "../images/testFace.png"
+import { useState } from "react";
 
 export default function About() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  
+  console.log(modalIsOpen); 
+
   return (
     <div className="aboutPage">
       <div className="title">
@@ -10,10 +29,10 @@ export default function About() {
         <h3>We're a small, closely-knit group of people based in Northern Virginia. We've been in business for over 3 years.</h3>
       </div>
       <div className="profileHorizontal">
-        <Profile name="Taran Kamireddy" position="Vice President" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit" image={testFace}></Profile>
-        <Profile name="Taran Kamireddy" position="Vice President" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit" image={testFace}></Profile>
-        <Profile name="Taran Kamireddy" position="Vice President" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit" image={testFace}></Profile>
+        <Profile openModal={openModal}  modalIsOpen={modalIsOpen} afterOpenModal={afterOpenModal} closeModal={closeModal} name="Taran Kamireddy" position="Vice President" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit" image={testFace}></Profile>
+
       </div>
     </div>
+    
   )
 }
